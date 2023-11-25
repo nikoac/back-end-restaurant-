cbb5d5bf7b1ae98922de2ef05717ccb85415b35c
CorreaJr <damiancorrea@gmail.com>
2023-11-25 20:55:31 -0300

    Modelo de usuarios hecho

---
+++ models/user.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Este campo es requerido'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Este campo es requerido'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Este campo es requerido'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Este campo es requerido'],
+    },
+    role: {
+        type: String,
+        required: [true, 'Este campo es requerido'],
+        enum: ['admin', 'client'],
+        default: 'client'
+    },
+    age: {
+        type: Number,
+        min: [16, 'La edad mínima requerida es de 16 años'],
+        max: [100, 'La edad máxima requerida es de 100 años']
+    },
+})
+
+module.exports = model('user', userSchema);
