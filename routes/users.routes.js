9a25ef6b68adc0999b2bd487b16541e1b9dbcf93
CorreaJr <damiancorrea@gmail.com>
2023-12-01 00:45:12 -0300

    Controladores de usuarios hechp

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -3,9 +3,13 @@ const { getAllUsers, createUser, getUserById, editUser, deleteUser } = require('
 const route = Router();
 
 route.get('/', getAllUsers);
+
 route.get('(byId/:id', getUserById);
+
 route.patch('/edit/:id', editUser);
+
 route.delete('/delete/:id', deleteUser);
+
 route.post('/create', createUser);
 
 module.exports = route;
