33a411a74d110ffae8e83096dcfb2ee963222d1b
CorreaJr <damiancorrea@gmail.com>
2023-12-13 19:34:09 -0300

    jsonwebtoken hecho

--- controllers/login.controllers.js
+++ controllers/login.controllers.js
@@ -1,12 +1,10 @@
+const { response } = require('express');
 const { passMatch } = require("../helpers/hashPassword");
-const { validateFields } = require("../helpers/utils");
 const { getUserByEmailService } = require("../services/user.services");
-
+const jwt = require('jsonwebtoken');
 
 const login = async (req, res) => {
   try {
-    const isValid = validateFields(req, res) 
-        if (!isValid) return ;
     const {email, password} = req.body;
     const response = await getUserByEmailService(email)
     if (!response) {
@@ -16,13 +14,18 @@ const login = async (req, res) => {
     if (!passwordMatchResult) {
       return res.status(200).json('El email o el password son incorrectos')
     }
-    const user = {
+    const payload = {
       name: response.name,
       lastName: response.lastName,
       email: response.email,
       role: response.role,
       _id: response._id
     }
+
+    const token = jwt.sign(payload, process.env.SECRET_KEY,{
+      expiresIn: 1,
+    });
+
     res.status(200).json(user)
   } catch (error) {
     res.status(500).json(error.message)
