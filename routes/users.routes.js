73cb90b4edc53f5cf6181cca70cb834d34738afd
TomasMoyano08 <tomii01moyano@gmail.com>
2024-04-20 16:57:15 -0300

    estoy cansado jefe

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -17,11 +17,7 @@ const { validateToken, validateRoll } = require("../middlewares/auth");
 
 const route = Router();
 
-<<<<<<< Updated upstream
 route.get("/", validateToken, validateRoll, getAllUsers);
-=======
-route.get("/", validateToken, getAllUsers);
->>>>>>> Stashed changes
 
 route.get("/byId/:id", getUserById);
 
@@ -29,14 +25,10 @@ route.get("/checkEmailExist",checkEmailExist);
 
 route.post("/create", ...createUserValidations, createUser);
 
-<<<<<<< Updated upstream
 route.patch("/edit/:id", ...editUserValidations, validateToken, editUser);
 
 route.delete("/delete/:id", /*...deleteUserValidations,*/ validateToken, validateRoll, deleteUser);
-=======
-route.patch("/edit/:id", validateToken, validateRoll, editUser);
->>>>>>> Stashed changes
 
-route.delete("/delete/:id",...deleteUserValidations, validateToken, validateRoll, deleteUser);
+
   
 module.exports = route;
--- validations/user.validations.js
+++ validations/user.validations.js
@@ -57,15 +57,10 @@ const validateAvatar = body('avatar')
 
 const editEmailValidation = body('email')
 .isEmail()
-<<<<<<< Updated upstream
 .withMessage("El campo email no es un email valido")
 .not()
 .isEmpty()
 .withMessage("El campo email es obligatorio")
-=======
-.withMessage('El campo email no es un email valido')
-.custom(checkEmailExistValidaton)
->>>>>>> Stashed changes
 
 
 const editPassword = body('password')
